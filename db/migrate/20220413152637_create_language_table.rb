b4fb5087c8632b82752160c5169c4e18a791eb28
ajlic24@gmail.com <ajlic24@gmail.com>
2022-04-13 12:09:17 -0400

    create seed data

--- db/migrate/20220413152637_create_language_table.rb
+++ db/migrate/20220413152637_create_language_table.rb
@@ -2,6 +2,7 @@ class CreateLanguageTable < ActiveRecord::Migration[6.1]
   def change
     create_table(:languages) do |t|
       t.column :name, :string
+      t.column :description, :string
     end
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,6 @@
 puts "🌱 Seeding spices..."
 
-# Seed your database here
+Language.create(id: 1, name: "Ruby", description: "Ruby is an Object-oriented language that is perfect for backend and API creation.")
+Language.create(id: 2, name: "Javascript", description: "Over 97% of websites use JavaScript on the client side for web page behavior, often incorporating third-party libraries. All major web browsers have a dedicated JavaScript engine to execute the code on users' devices, making it a language for frontend web development.")
 
 puts "✅ Done seeding!"
