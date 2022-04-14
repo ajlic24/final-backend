2153d85e85ba8536082f4fb6b9e614f4390892ab
ajlic24@gmail.com <ajlic24@gmail.com>
2022-04-13 20:02:39 -0400

    create post route

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,22 @@
 class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
   
-  # Add your routes here
-  get "/" do
-    { message: "Good luck with your project!" }.to_json
+  get "/ruby" do
+    ruby = Language.find_by(name: 'Ruby').commands
+    ruby.to_json(only: [:id, :name, :description])
+  end
+
+  get "/javascript" do
+    ruby = Language.find_by(name: 'Javascript').commands
+    ruby.to_json(only: [:id, :name, :description])
+  end
+
+  post "/ruby" do
+    Command.create(name: params[:name], description: params[:description], language: Language.find_by(name: "Ruby"))
+  end
+
+  post "/javascript" do
+    Command.create(name: params[:name], description: params[:description], language: Language.find_by(name: "Javascript"))
   end
 
 end
